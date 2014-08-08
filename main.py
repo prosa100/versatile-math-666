9eeab766a78a283235710416452ce86a2a8518d9
Paul Rosa <prosa100@gmail.com>
2014-08-08 22:11:04 +0000

    go python

---
+++ main.py
@@ -0,0 +1,13 @@
+import webapp2
+
+
+class MainHandler(webapp2.RequestHandler):
+
+  def get(self):  # pylint:disable-msg=invalid-name
+    """Handle GET requests."""
+    self.response.write("""Hello, world.""")
+
+
+APP = webapp2.WSGIApplication([
+    ('/.*', MainHandler),
+], debug=True)
