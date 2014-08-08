46283c79edc5f36272aaa66765f0c999ebf1fca3
Paul Rosa <prosa100@gmail.com>
2014-08-08 21:56:28 +0000

    hello

--- hello.go
+++ hello.go
@@ -11,9 +11,4 @@ func init() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello, world!")
-    sum=0
-    for i := 0; i < 1000000; i++ {
-        sum += i
-    }
-    fmt.Fprint(w, "Hello, world!", sum)
 }
