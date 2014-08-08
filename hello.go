5de95b957704bdf9b5a0c0e9bec4975ba4a50132
Paul Rosa <prosa100@gmail.com>
2014-08-08 21:18:47 +0000

    added hello world

---
+++ hello.go
@@ -0,0 +1,18 @@
+package hello
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, world!")
+    for i := 0; i < 1000000; i++ {
+        sum += i
+    }
+    fmt.Fprint(w, "Hello, world!", sum)
+}
